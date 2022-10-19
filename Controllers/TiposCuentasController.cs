84209ed751c18d3be7b32b0f986edb00a0d05047
Felix <52052988+FeJoToRe@users.noreply.github.com>
2022-10-19 11:04:48 -0400

    Add project files.

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TiposCuentasController tiposCuentas)
+        {
+            if (!ModelState.IsValid) {
+                return View(tiposCuentas);
+            }
+
+            return View();
+
+        }
+
+    }
+}
---
+++ Models/TipoCuentaModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuentaModel
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "No se puede dejar el campo vacio")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
