01e13a775334cb85eed5d43da3115e6a842736fd
Felix <52052988+FeJoToRe@users.noreply.github.com>
2022-11-01 19:28:41 -0400

    modificado hasta la clase 114, agregada clase repositorio para conectarse a la DB y manejar los datos; agregada interfaz de repositorio

--- Models/TipoCuentaModel.cs
+++ Models/TipoCuentaModel.cs
@@ -4,8 +4,9 @@ namespace ManejoPresupuesto.Models
 {
     public class TipoCuentaModel
     {
-        public int ID { get; set; }
         [Required(ErrorMessage = "No se puede dejar el campo vacio")]
+        [StringLength(maximumLength: 100, MinimumLength = 10, ErrorMessage = "El mensaje debe contener entre {2} y {1} caracteres")]
+        public int ID { get; set; }
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
